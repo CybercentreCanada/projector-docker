df557f7ddecb70b113f1fd89425dedfa533cbe51
cccs-tom <59839056+cccs-tom@users.noreply.github.com>
2021-10-26 16:19:49 -0400

    Add user feedback to launcher script

--- run-trino-env.sh
+++ run-trino-env.sh
@@ -1,16 +1,32 @@
 #!/bin/sh
 
-set -e # Any command which returns non-zero exit code will cause this shell script to exit immediately
-set -x # Activate debugging to show execution details: all commands will be printed before execution
+trinoCloneDir=${1:-~/repositories/trino}
 
+echo "Checking Azure login status"
 az account show >/dev/null
 AZ_LOGIN_STATUS=$?
-if [ $AZ_LOGIN_STATUS -ne 0 ]; then
+if [ $AZ_LOGIN_STATUS -eq 0 ]; then
+  echo "Already logged in to Azure"
+else
+  echo "Attempting Azure login"
   az login
+  AZ_LOGIN_STATUS=$?
+  if [ $AZ_LOGIN_STATUS -eq 0 ]; then
+    echo "Successfully logged in to Azure"
+  else
+    echo "Azure login failed"
+    exit 1
+  fi
 fi
 
+echo "Logging in to Azure Container Registry"
 az acr login --name uchimera >/dev/null
 ACR_LOGIN_STATUS=$?
 if [ $ACR_LOGIN_STATUS -eq 0 ]; then
-  docker run --rm --pull always -p 8887:8887 -v ~/workspaces/trino:/home/projector-user:cached -v ~/repositories/trino:/workspace/trino:cached -it uchimera.azurecr.io/cccs/dev/projector-intellij-ce:trino
+  mkdir -p ~/workspaces/trino
+  echo "Starting IDE"
+  docker run --rm --pull always -p 8887:8887 -v ~/workspaces/trino:/home/projector-user:cached -v ${trinoCloneDir}:/workspace/trino:cached -it uchimera.azurecr.io/cccs/dev/projector-intellij-ce:trino
+else
+  echo "Azure Container Registry login failed"
+  exit 1
 fi
