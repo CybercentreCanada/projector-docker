1f21ec4e91bc326d25b679f42f95778dc0769fca
cccs-tom <59839056+cccs-tom@users.noreply.github.com>
2021-11-16 17:20:08 +0000

    Switch to bind mounts instead of volumes

--- run-trino-env.sh
+++ run-trino-env.sh
@@ -5,13 +5,13 @@ trinoCloneDir=${1:-~/repositories/trino}
 echo "Checking Azure login status"
 az account show >/dev/null
 AZ_LOGIN_STATUS=$?
-if [ $AZ_LOGIN_STATUS -eq 0 ]; then
+if [ ${AZ_LOGIN_STATUS} -eq 0 ]; then
   echo "Already logged in to Azure"
 else
   echo "Attempting Azure login"
   az login
   AZ_LOGIN_STATUS=$?
-  if [ $AZ_LOGIN_STATUS -eq 0 ]; then
+  if [ ${AZ_LOGIN_STATUS} -eq 0 ]; then
     echo "Successfully logged in to Azure"
   else
     echo "Azure login failed"
@@ -22,10 +22,10 @@ fi
 echo "Logging in to Azure Container Registry"
 az acr login --name uchimera >/dev/null
 ACR_LOGIN_STATUS=$?
-if [ $ACR_LOGIN_STATUS -eq 0 ]; then
+if [ ${ACR_LOGIN_STATUS} -eq 0 ]; then
   mkdir -p ~/workspaces/trino
   echo "Starting IDE"
-  docker run --rm --pull always -p 8887:8887 -v ~/workspaces/trino:/home/projector-user:cached -v ${trinoCloneDir}:/workspace/trino:cached -it uchimera.azurecr.io/cccs/dev/projector-intellij-ce:trino
+  docker run --rm --pull always -p 8887:8887 --mount type=bind,source="${HOME}/workspaces/trino",target=/home/projector-user --mount type=bind,source="${trinoCloneDir}",target=/workspace/trino -it uchimera.azurecr.io/cccs/dev/projector-intellij-ce:trino
 else
   echo "Azure Container Registry login failed"
   exit 1
