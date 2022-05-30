748840cb3f6b55b9929c55bd1aeb7e7af2bc7a17
cccs-tom <59839056+cccs-tom@users.noreply.github.com>
2022-05-30 15:31:24 -0400

    [CLDN-1118] Improve docker registry login to match our dev env

--- run-trino-env.sh
+++ run-trino-env.sh
@@ -5,13 +5,13 @@ trinoCloneDir=${1:-~/repositories/trino}
 echo "Checking Azure login status"
 az account show >/dev/null
 AZ_LOGIN_STATUS=$?
-if [ ${AZ_LOGIN_STATUS} -eq 0 ]; then
+if [[ ${AZ_LOGIN_STATUS} -eq 0 ]]; then
   echo "Already logged in to Azure"
 else
   echo "Attempting Azure login"
   az login
   AZ_LOGIN_STATUS=$?
-  if [ ${AZ_LOGIN_STATUS} -eq 0 ]; then
+  if [[ ${AZ_LOGIN_STATUS} -eq 0 ]]; then
     echo "Successfully logged in to Azure"
   else
     echo "Azure login failed"
@@ -20,21 +20,19 @@ else
 fi
 
 echo "Logging in to Azure Container Registry"
-#read -r LOGIN_SERVER ACCESS_TOKEN <<< $(az acr login -n uchimera --expose-token --query '[loginServer, accessToken]' -otsv 2>/dev/null)
-read -r ACCESS_TOKEN LOGIN_SERVER <<< $(az acr login -n uchimera --expose-token  -otsv 2>/dev/null)
+read -r LOGIN_SERVER ACCESS_TOKEN < <(az acr login -n uchimera --expose-token --query "join(' ', [loginServer, accessToken])" -otsv 2>/dev/null)
 ACR_LOGIN_STATUS=$?
-if [ ${ACR_LOGIN_STATUS} -eq 0 ]; then
+if [[ ${ACR_LOGIN_STATUS} -eq 0 ]]; then
   echo "Logging in to Docker Registry"
-#  docker login ${LOGIN_SERVER} -u 00000000-0000-0000-0000-000000000000 -p ${ACCESS_TOKEN}
-  docker login $(echo ${LOGIN_SERVER} | tr -d '\r') -u 00000000-0000-0000-0000-000000000000 -p ${ACCESS_TOKEN}
+  docker login ${LOGIN_SERVER} -u 00000000-0000-0000-0000-000000000000 --password-stdin <<< ${ACCESS_TOKEN}
   DOCKER_LOGIN_STATUS=$?
-  if [ ${DOCKER_LOGIN_STATUS} -eq 0 ]; then
+  if [[ ${DOCKER_LOGIN_STATUS} -eq 0 ]]; then
     mkdir -p ~/workspaces/trino
     echo "Starting IDE"
     docker run --rm --pull always -p 8887:8887 --mount type=bind,source="${HOME}/workspaces/trino",target=/home/projector-user --mount type=bind,source="${trinoCloneDir}",target=/workspace/trino -it uchimera.azurecr.io/cccs/dev/projector-intellij-ce:trino
   else
-  echo "Docker login failed"
-  exit 1
+    echo "Docker login failed"
+    exit 1
   fi
 else
   echo "Azure Container Registry login failed"
