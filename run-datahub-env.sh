0a34f221af673265a6b1caeb18d40791701fd812
cccs-tom <59839056+cccs-tom@users.noreply.github.com>
2022-08-22 20:38:57 +0000

    [CLDN-1571] Add run-script for datahub

---
+++ run-datahub-env.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+sourceDir=${1:-~/repositories/cccs-work/datahub}
+
+echo "Checking Azure login status"
+az account show >/dev/null
+AZ_LOGIN_STATUS=$?
+if [[ ${AZ_LOGIN_STATUS} -eq 0 ]]; then
+  echo "Already logged in to Azure"
+else
+  echo "Attempting Azure login"
+  az login
+  AZ_LOGIN_STATUS=$?
+  if [[ ${AZ_LOGIN_STATUS} -eq 0 ]]; then
+    echo "Successfully logged in to Azure"
+  else
+    echo "Azure login failed"
+    exit 1
+  fi
+fi
+
+echo "Logging in to Azure Container Registry"
+read -r LOGIN_SERVER ACCESS_TOKEN < <(az acr login -n uchimera --expose-token --query "join(' ', [loginServer, accessToken])" -otsv 2>/dev/null)
+ACR_LOGIN_STATUS=$?
+if [[ ${ACR_LOGIN_STATUS} -eq 0 ]]; then
+  echo "Logging in to Docker Registry"
+  docker login ${LOGIN_SERVER} -u 00000000-0000-0000-0000-000000000000 --password-stdin <<< ${ACCESS_TOKEN}
+  DOCKER_LOGIN_STATUS=$?
+  if [[ ${DOCKER_LOGIN_STATUS} -eq 0 ]]; then
+    mkdir -p ~/workspaces/datahub
+    echo "Starting IDE"
+    docker run --rm --pull always -p 8887:8887 --mount type=bind,source="${HOME}/workspaces/datahub",target=/home/projector-user --mount type=bind,source="${sourceDir}",target=/workspace/datahub -it uchimera.azurecr.io/cccs/dev/projector-intellij-ce:datahub
+  else
+    echo "Docker login failed"
+    exit 1
+  fi
+else
+  echo "Azure Container Registry login failed"
+  exit 1
+fi
